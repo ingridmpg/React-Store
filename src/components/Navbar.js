0142aa75c8fbc226634234b30dcf8c0ba3ae0f25
ingridmpg <ingrid.grelo@gmail.com>
2023-10-18 22:32:24 +0100

    Create new Brand component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import logo from "../logo.svg";
 import styled from "styled-components";
 import { ButtonContainer } from "./Button";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -10,15 +9,9 @@ export default class Navbar extends Component {
     render() {
         return (
             <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
-                {/* https://www.iconfinder.com/icons/1243689/call_phone_icon
-                Creative Commons (Attribution 3.0 Unported);
-                https://www.iconfinder.com/Makoto_msk */}
-                <Link to="/">
-                    <img src={logo} alt="store" className="navbar-brand" />
-                </Link>
                 <ul className="navbar-nav align-items-center">
                     <li className="nav-item ml-5">
-                        <Link to="/" className="nav-link">
+                        <Link to="/" className="nav-link text-navbar">
                             products
                         </Link>
                     </li>
@@ -38,8 +31,7 @@ export default class Navbar extends Component {
 const NavWrapper = styled.nav`
     background: var(--mainGray);
     .nav-link {
-        color: var(--mainWhite) !important;
+        color: var(--black) !important;
         font-size: 1.3rem;
-        text-transform: capitalize;
     }
 `;
